0275310d792f89f7b84fe31dc516fd2f15cb3933
Simon Reymann <simon.reymann@gmail.com>
2017-08-10 17:26:06 +0200

    support params for release task (bumpMode, devBranch, prodBranch, buildCommand)

--- scripts/patch-n-publish.sh
+++ scripts/patch-n-publish.sh
@@ -4,11 +4,17 @@
 currentBranch=$(git rev-parse --abbrev-ref HEAD)
 
 echo "CurrentBranch: $currentBranch"
+
+if [ -z "$PNP_DEVBRANCH" ]; then
+    PNP_DEVBRANCH=$currentBranch
+fi
+
 echo "SetupDevBranch: $PNP_DEVBRANCH"
 echo "SetupProdBranch: $PNP_PRODBRANCH"
 echo "BuidCommand: $PNP_BUILDCOMMAND"
+echo "BumpMode: $1"
 
-#If we are not in develop, we need to switch to it
+#If we are not in DEVBRANCH, we need to switch to it
 if [ -n "$currentBranch" -a "$currentBranch" != "$PNP_DEVBRANCH" ]; then
 	echo "switch back to $PNP_DEVBRANCH branch..."
 	git checkout $PNP_DEVBRANCH
@@ -16,66 +22,77 @@ if [ -n "$currentBranch" -a "$currentBranch" != "$PNP_DEVBRANCH" ]; then
 	git pull
 fi
 
-echo "pull changes from master"
-git pull origin $PNP_PRODBRANCH
+if [ -n "$PNP_PRODBRANCH" ]; then
+    echo "pull changes from $PNP_PRODBRANCH"
+    git pull origin $PNP_PRODBRANCH
+fi
 
-#build in develop
-echo "do build in develop"
+#build in DEVBRANCH
+echo "do build in $PNP_DEVBRANCH"
 $PNP_BUILDCOMMAND
 
-#Test if we hav any difference between develop and master
+#Test if we hav any difference between local and origin
 if [[ $(git diff --name-status $PNP_DEVBRANCH) ]]; then
-    echo "differences origin develop and local"
-    echo "commit in develop"
+    echo "differences origin $PNP_DEVBRANCH and local $PNP_DEVBRANCH"
+    echo "commit in $PNP_DEVBRANCH"
     git add *
     git commit -m 'build dist'
     git push
 else
-    echo "no differences between develop and master found"
+    echo "no differences between origin $PNP_DEVBRANCH and local $PNP_DEVBRANCH found"
 fi
 
-#Test if we hav any difference between develop and master
-if [[ $(git diff --name-status $PNP_PRODBRANCH..$PNP_DEVBRANCH) ]]; then
-    echo "differences between develop and master found"
-else
-    echo "no differences between develop and master found"
-    exit 0; 
+if [ -n "$PNP_PRODBRANCH" ]; then
+
+    #Test if we hav any difference between DEVBRANCH and PRODBRANCH
+    if [[ $(git diff --name-status $PNP_PRODBRANCH..$PNP_DEVBRANCH) ]]; then
+        echo "differences between $PNP_DEVBRANCH and $PNP_PRODBRANCH found"
+    else
+        echo "no differences between $PNP_DEVBRANCH and $PNP_PRODBRANCH found"
+        exit 0;
+    fi
+
+    #Switch to PRODBRANCH
+    echo "switch to $PNP_PRODBRANCH branch..."
+    git checkout $PNP_PRODBRANCH
+
+    #Merge DEVBRANCH into PRODBRANCH
+    echo "merge $PNP_DEVBRANCH into $PNP_PRODBRANCH branch..."
+    git merge $PNP_DEVBRANCH
+
 fi
 
-#Switch to master
-echo "switch to master branch..."
-git checkout $PNP_PRODBRANCH
-
-#Merge develop into master
-echo "merge develop into master branch..."
-git merge $PNP_DEVBRANCH
-
-#Then we bump the version in the developing branch
+#Then we bump the version in the DEVBRANCHing branch
 if [ -n "$1" -a "$1" = "major" ]; then
-	echo "Bump major version number in release (x).x.x!"
-	npm version major -m "Bumped (via major) to version:%s"
+	echo "Bump major version number in $PNP_DEVBRANCH (x).x.x!"
+    npm version major -m "Bumped (via major) to version:%s"
 elif [ -n "$1" -a "$1" = "minor" ]; then
-	echo "Bump minor version number in release x.(x).x!"
+	echo "Bump minor version number in $PNP_DEVBRANCH x.(x).x!"
 	npm version minor -m "Bumped (via minor) to version:%s"
-else
-	echo "Bump patch version number in develop x.x.(x)!"
+elif [ -z "$1" -o "$1" = "patch" ]; then
+	echo "Bump patch version number in $PNP_DEVBRANCH x.x.(x)!"
 	npm version patch -m "Bumped (via patch) to version:%s"
+else
+	echo "Bump $1 version number in $PNP_DEVBRANCH!"
+	npm version $1 -m "Bumped (via $1) to version:%s"
 fi
 
-echo "push new package.json with tag into master"
+echo "push new package.json with tag"
 git push --tags
 git push
 
 echo "Publish to npm registry"
 npm publish
 
-#Switch to develop
-echo "switch back to develop branch..."
-git checkout $PNP_DEVBRANCH
+if [ -n "$PNP_PRODBRANCH" ]; then
+    #Switch to DEVBRANCH
+    echo "switch back to $PNP_DEVBRANCH branch..."
+    git checkout $PNP_DEVBRANCH
 
-#Merge master into develop
-echo "merge master into develop branch..."
-git merge $PNP_PRODBRANCH
-git push
+    #Merge PRODBRANCH into DEVBRANCH
+    echo "merge $PNP_PRODBRANCH into $PNP_DEVBRANCH branch..."
+    git merge $PNP_PRODBRANCH
+    git push
+fi
 
 exit 0;
--- src/tasks/release.ts
+++ src/tasks/release.ts
@@ -1,17 +1,36 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import * as shell from 'gulp-shell';
+const yargs = require('yargs');
 
 function generate(gulp, config, gulptraum) {
 
   gulp.task('release', () => {
-  
-    const releasePnpBuildCommand = ((config && config.releasePnpBuildCommand) || 'gulp build');
-    const releasePnpDevBranch = ((config && config.releasePnpDevBranch) || 'develop');
-    const releasePnpProdBranch = ((config && config.releasePnpProdBranch) || 'master');
-  
+
+    let releasePnpBuildCommand = ((config && config.releasePnpBuildCommand) || 'gulp build');
+    let releasePnpDevBranch = ((config && config.releasePnpDevBranch) || 'develop');
+    let releasePnpProdBranch = ((config && config.releasePnpProdBranch) || 'master');
+    let versioningMethod = '';
+
+    const argv = yargs.argv;
+    if (argv && argv.devBranch) {
+      releasePnpDevBranch = argv.devBranch;
+    }
+    if (argv && argv.prodBranch) {
+      releasePnpProdBranch = argv.prodBranch;
+    }
+    if (argv && argv.buildCommand) {
+      releasePnpBuildCommand = argv.buildCommand;
+    }
+    if (argv && argv.bumpMode) {
+      versioningMethod = ' ' + argv.bumpMode;
+      if (argv.bumpMode.indexOf('pre') === 0) {
+        releasePnpDevBranch = '';
+        releasePnpProdBranch = '';
+      }
+    }
+
     gulp.task('release', shell.task([
-      'PNP_DEVBRANCH=' + releasePnpDevBranch + ' PNP_PRODBRANCH=' + releasePnpProdBranch + ' PNP_BUILDCOMMAND="' + releasePnpBuildCommand + '" sh ' + path.resolve(__dirname, '../../../scripts/patch-n-publish.sh')
+      'PNP_DEVBRANCH=' + releasePnpDevBranch + ' PNP_PRODBRANCH=' + releasePnpProdBranch + ' PNP_BUILDCOMMAND="' + releasePnpBuildCommand + '" sh ' + path.resolve(__dirname, 'scripts/patch-n-publish.sh' + versioningMethod)
     ]));
   });
 }
